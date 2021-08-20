af0c26d5b5bea5249b10691d98ace6d4c1ed5224
Ikimune <s-kaho.ikimune@rakuten.com>
2021-08-20 15:45:50 +0900

    .\.git\第2章 - PHP実践 - 第1部 - 変数とデータ型

--- php01.php
+++ php01.php
@@ -7,14 +7,14 @@ echo $a + $b;
 <br></br>
 <?php
 $array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
-echo $array_month[8];
+echo $array_month[7];
 ?>
 <br></br>
 <?php
 $hello = "”Hello,";
 $name = "KAHO";
 $world = "’s World!";
-echo $hello; echo $name; echo $world;
+echo $hello.$name.$world;
 ?>
 <br></br>
 <?php
